9a6fd0d0126f3c881bfda6aeeead7c5cddb2404c
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-07-17 09:52:28 +0400

    Initial Commit

---
+++ bucky/src/bucky/apples.java
@@ -0,0 +1,51 @@
+package bucky;
+
+class apples{
+
+	public static void main(String args[]){
+		
+		
+		int counter = 0;
+		
+		while (counter<10)
+		{
+			System.out.println(counter);
+			counter++;
+		}
+		
+//		int age;
+//		age = 7;
+//		
+//		switch(age){
+//		case 1:
+//			System.out.println("You can crawl");
+//			break;
+//		case 2:
+//			System.out.println("You can talk");
+//			break;
+//		case 3:
+//			System.out.println("You can get in trouble");
+//			break;
+//		default:
+//			System.out.println("I dont know how old you are");
+//			break;
+//		
+//		}
+		
+		
+		
+//		int boy,girl;
+//		boy = 18;
+//		girl = 68;
+//		
+//		if (boy >10 && girl<60){
+//			
+//			System.out.println("You can enter");
+//						
+//		}
+//		else{
+//			System.out.println("You cannot enter");
+//		}
+//		
+	}
+}
